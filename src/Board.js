042b7a4693e7a1dbdff717eb3c85d923086e619d
Stanislav Kozin <stas@staskozin.ru>
2019-10-08 17:10:59 +0300

    Rewrite the Board component using nested loops

--- src/Board.js
+++ src/Board.js
@@ -7,27 +7,26 @@ class Board extends React.Component {
     return <Square 
               value={this.props.squares[i]}
               onClick={() => this.props.onClick(i)}
-            />
+              key={i}
+           />
+  }
+
+  renderRows () {
+    let rows = []
+    let index = 0
+    for (let i = 0; i < 3; i++) {
+      let squares = []
+      for (const m = index + 3; index < m; index++)
+        squares.push(this.renderSquare(index))
+      rows.push(<div key={i} className="board-row">{squares}</div>)
+    }
+    return rows
   }
 
   render () {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {this.renderRows()}
       </div>
     )
   }
